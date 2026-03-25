c00d908edff9d094fe2a28052901cce4009c5514
Anthony Alanya <anthonyalanya088@gmail.com>
2026-03-25 11:05:48 -0500

    scenarios into password validator

---
+++ src/main/java/edu/pe/cibertec/PasswordValidator.java
@@ -0,0 +1,29 @@
+package edu.pe.cibertec;
+
+public class PasswordValidator {
+
+    private static final int MIN_LENGHT = 7;
+    private static final String SPECIAL_CHARS = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+
+    public boolean isValid(String password) {
+        if (password == null || password.length() < MIN_LENGHT) return false;
+        if (password.contains(" ")) return false;
+        if (!hasUpperCase(password)) return false;
+        if (!hasLowerCase(password)) return false;
+        if (!hasDigit(password)) return false;
+        return true;
+    }
+
+    private boolean hasUpperCase(String password) {
+        return password.chars().anyMatch(Character::isUpperCase);
+    }
+
+    private boolean hasLowerCase(String password) {
+        return password.chars().anyMatch(Character::isLowerCase);
+    }
+
+    private boolean hasDigit(String password) {
+        return password.chars().anyMatch(Character::isDigit);
+    }
+
+}
