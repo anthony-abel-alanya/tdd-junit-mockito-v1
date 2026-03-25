f22f8fab262f25665c264cc846513a66fb2bcbc8
Anthony Alanya <anthonyalanya088@gmail.com>
2026-03-25 10:31:14 -0500

    First Commit

---
+++ src/test/java/edu/pe/cibertec/LeapYearTest.java
@@ -0,0 +1,41 @@
+package edu.pe.cibertec;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("LeapYear - Junit Implementation")
+public class LeapYearTest {
+    private LeapYear leapYear;
+
+    @BeforeEach
+    void setUp(){
+        leapYear = new LeapYear();
+    }
+
+    @Test
+    @DisplayName("Year Divisible by 4")
+    void giveYearDivisibleBy4_whenIsLeapYear_thenTrue(){
+        assertTrue(leapYear.isLeapYear(2024));
+    }
+
+    @Test
+    @DisplayName("Year Divisible by 400")
+    void giveYearDivisibleBy400_whenIsLeapYear_thenTrue(){
+        assertTrue(leapYear.isLeapYear(2000));
+    }
+
+    @Test
+    @DisplayName("Year is Not Divisible by 4")
+    void givenYearNotDivisibleBy4_whenIsYear_thenFalse(){
+        assertFalse(leapYear.isLeapYear(2025));
+    }
+
+    @Test
+    @DisplayName("Negative year throws exception")
+    void givenYearNotDivisibleBy4_whenIsYear_thenThrowsException(){
+        assertThrows(IllegalArgumentException.class, () -> leapYear.isLeapYear(-20));
+    }
+}
